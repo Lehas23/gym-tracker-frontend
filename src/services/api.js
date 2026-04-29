94ed5e7145e5ad76812c045a03a85c980650d2b2
Leart <le.hasanaj@gmail.com>
2026-04-29 10:49:03 +0200

    prepare for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://localhost:7149'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 api.interceptors.request.use((config) => {
